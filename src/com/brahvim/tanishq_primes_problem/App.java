b5040978b81fa0695ffb559bb2224472113ed4f3
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-01-20 22:30:17 +0530

    Wow, old really is gold... The older version of the algorithm should be faster according to asymptotic analysis...
    Should still bench 'em, bois and gals!

--- src/com/brahvim/tanishq_primes_problem/App.java
+++ src/com/brahvim/tanishq_primes_problem/App.java
@@ -2,14 +2,42 @@ package com.brahvim.tanishq_primes_problem;
 
 public class App {
     public static void main(final String[] args) {
-        System.out.println(App.isHaningNumber(6));
-        System.out.println(App.isHaningNumber(90));
-        System.out.println(App.isHaningNumber(44));
+        System.out.println(App.isHaningNumberOld(6));
+        System.out.println(App.isHaningNumberOld(90));
+        System.out.println(App.isHaningNumberOld(44));
 
         // Extra test case
         // (this is why I use `return p_number != number;`,
         // and not just `return true;`)!:
-        System.out.println(App.isHaningNumber(13));
+        System.out.println(App.isHaningNumberOld(13));
+    }
+
+    public static boolean isHaningNumberOld(final int p_number) {
+        int number = p_number;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i != 0)
+                continue;
+
+            number /= i;
+
+            // if (!(i == 2 || i == 3 || i == 5))
+            // return false;
+
+            // This should be faster if you use `int`s.
+            // I'm using `long`s because I think user input should not have a small limit:
+
+            switch (i) {
+                case 2:
+                case 3:
+                case 5:
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+        return number != p_number;
     }
 
     /**
