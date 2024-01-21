d1708b100dc4c97d9bca840e23f45675c7ed2011
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-01-21 07:14:20 +0530

    This uses `main()`, and `main()` only!
    Thank C macros using do-while loops and not just code blocks all the time! That's what reminded me how to pull this off!

--- src/com/brahvim/tanishq_primes_problem/App.java
+++ src/com/brahvim/tanishq_primes_problem/App.java
@@ -1,64 +1,52 @@
 package com.brahvim.tanishq_primes_problem;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(final String[] args) {
-        System.out.println("Original test cases:");
+        System.out.print("Please enter a number to check: ");
 
-        System.out.printf("`6` is %s haning number.%n", App.isHaningNumber(6) ? "a" : "not a");
-        System.out.printf("`90` is %s haning number.%n", App.isHaningNumber(90) ? "a" : "not a");
-        System.out.printf("`44` is %s haning number.%n", App.isHaningNumber(44) ? "a" : "not a");
+        final Scanner sc = new Scanner(System.in); // Make a `Scanner` to read `System.in`.
+        final int number = sc.nextInt(); // Read an `int` with it,
+        sc.close(); // Delete all memory `sc` used.
 
-        System.out.printf("%n%nExtra test cases:%n");
-        // Extra test case
-        // (this is why I use `return p_number != number;`,
-        // and not just `return true;`)!:
-        System.out.printf("`13` is %s haning number.%n", App.isHaningNumber(13) ? "a" : "not a");
-        System.out.printf("`-1` is %s haning number.%n", App.isHaningNumber(-1) ? "a" : "not a");
+        boolean isHaning;
 
-        System.out.printf("%n%nFrom `1` to `10`:%n");
-        for (int i = 1; i < 11; i++)
-            System.out.printf("%d) `%s`.%n", i, App.isHaningNumber(i));
+        // I'm using a do-while loop so I can use `break` instead of `return`!
+        // This is a trick that C taught me :> (C macros, to be exact).
+        do {
+            if (number < 2) { // `1` is not a haning number either!
+                isHaning = false;
+                break;
+            }
 
-        /*
-         * From `1` to `10`:
-         * 1) `false`.
-         * 2) `true`.
-         * 3) `true`.
-         * 4) `true`.
-         * 5) `true`.
-         * 6) `true`.
-         * 7) `false`.
-         * 8) `true`.
-         * 9) `true`.
-         * 10) `true`.
-         */
-    }
+            // Shortcut/Optimization plus `{2, 3, 5} => true` fix:
+            if (number < 7) {
+                isHaning = true;
+                break;
+            }
 
-    public static boolean isHaningNumber(final int p_number) {
-        if (p_number < 2) // `1` is not a haning number either!
-            return false;
+            for (int i = 2; i < number; i++) {
+                if (number % i != 0)
+                    continue;
 
-        // Shortcut/Optimization plus `{2, 3, 5} => true` fix:
-        if (p_number < 7)
-            return true;
+                // This should be faster if you use `int`s.
+                // I'm using `long`s because I think user input should not have a small limit:
 
-        for (int i = 2; i < p_number; i++) {
-            if (p_number % i != 0)
-                continue;
+                // isHaning = switch (i) {
+                // case 2, 3, 5 -> true;
+                // default -> false;
+                // };
+                // break;
 
-            // A `switch` is MUCH faster than an `if`. If you the JVM's bytecode, you'll
-            // see (I actually tried this on [ https://godbolt.org ]!):
-            return switch (i) {
-                case 2, 3, 5 -> true;
-                default -> false;
-            };
+                isHaning = i == 2 || i == 3 || i == 5;
+                break;
+            }
 
-            // This is necessary when using `long`s/`float`s`/`double`s.
-            // Don't forget to suffix the numbers with `L`/`f`/`d`!:
-            // return i == 2 || i == 3 || i == 5;
-        }
+            isHaning = false;
+        } while (false);
 
-        return false;
+        System.out.println(isHaning);
     }
 
 }
