84e00b88f62cad7f89acad1d2db9fd5fd78434e5
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-01-21 06:34:13 +0530

    This way of doing it is EXTREMELY stupid, but it works...?!

--- src/com/brahvim/tanishq_primes_problem/App.java
+++ src/com/brahvim/tanishq_primes_problem/App.java
@@ -4,20 +4,20 @@ public class App {
     public static void main(final String[] args) {
         System.out.println("Original test cases:");
 
-        System.out.printf("`6` is %s haning number.%n", App.isHaningNumberOld(6) ? "a" : "not a");
-        System.out.printf("`90` is %s haning number.%n", App.isHaningNumberOld(90) ? "a" : "not a");
-        System.out.printf("`44` is %s haning number.%n", App.isHaningNumberOld(44) ? "a" : "not a");
+        System.out.printf("`6` is %s haning number.%n", App.isHaningNumber(6) ? "a" : "not a");
+        System.out.printf("`90` is %s haning number.%n", App.isHaningNumber(90) ? "a" : "not a");
+        System.out.printf("`44` is %s haning number.%n", App.isHaningNumber(44) ? "a" : "not a");
 
         System.out.printf("%n%nExtra test cases:%n");
         // Extra test case
         // (this is why I use `return p_number != number;`,
         // and not just `return true;`)!:
-        System.out.printf("`13` is %s haning number.%n", App.isHaningNumberOld(13) ? "a" : "not a");
-        System.out.printf("`-1` is %s haning number.%n", App.isHaningNumberOld(-1) ? "a" : "not a");
+        System.out.printf("`13` is %s haning number.%n", App.isHaningNumber(13) ? "a" : "not a");
+        System.out.printf("`-1` is %s haning number.%n", App.isHaningNumber(-1) ? "a" : "not a");
 
         System.out.printf("%n%nFrom `1` to `10`:%n");
         for (int i = 1; i < 11; i++)
-            System.out.printf("%d) `%s`.%n", i, App.isHaningNumberOld(i));
+            System.out.printf("%d) `%s`.%n", i, App.isHaningNumber(i));
 
         /*
          * From `1` to `10`:
@@ -34,7 +34,7 @@ public class App {
          */
     }
 
-    public static boolean isHaningNumberOld(final int p_number) {
+    public static boolean isHaningNumber(final int p_number) {
         if (p_number < 2) // `1` is not a haning number either!
             return false;
 
@@ -46,10 +46,16 @@ public class App {
             if (p_number % i != 0)
                 continue;
 
-            // This should be faster if you use `int`s.
-            // I'm using `long`s because I think user input should not have a small limit:
+            // A `switch` is MUCH faster than an `if`. If you the JVM's bytecode, you'll
+            // see (I actually tried this on [ https://godbolt.org ]!):
+            return switch (i) {
+                case 2, 3, 5 -> true;
+                default -> false;
+            };
 
-            return i == 2 || i == 3 || i == 5;
+            // This is necessary when using `long`s/`float`s`/`double`s.
+            // Don't forget to suffix the numbers with `L`/`f`/`d`!:
+            // return i == 2 || i == 3 || i == 5;
         }
 
         return false;
